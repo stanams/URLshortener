13bc48d1a5072eb9beb9b77ec8b45d3e8d7f6617
‘stanams’ <stan.amsellem@gmail.com;GIT_COMMITTER_NAME=stanams;GIT_COMMITTER_EMAIL=stan.amsellem@gmail.com>
2016-01-20 18:06:52 -0800

    removes unnecessary files

--- aa_questions/manifest.rb
+++
@@ -1,6 +0,0 @@
-require_relative 'supermodel'
-require_relative 'question'
-require_relative 'question_follows'
-require_relative 'replies'
-require_relative 'users'
-require_relative 'question_likes'
